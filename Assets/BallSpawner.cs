725ea8327cd3a54b6be0aef75fbdaf40d6b81e47
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2022-06-20 16:18:53 +0700

    init

---
+++ Assets/BallSpawner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public Vector3 spawnDirection;
+
+    public void SpawnBall()
+    {
+
+    }
+}
